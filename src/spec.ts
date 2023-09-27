ad43b167fb9bcd3e96d22acd917eaecbe06d0b78
Guilherme Bernal <guilherme@cubos.io>
2023-09-27 10:49:53 -0300

    feat: more specs

--- src/spec.ts
+++ src/spec.ts
@@ -4,7 +4,7 @@ import { ParseRinha } from "./parser";
 
 type Rinha<Source extends string> = ParseRinha<Source> extends infer Ast ? Ast extends { expression: any } ? ExecuteRinha<Ast> : Ast : never;
 type RinhaValue<Source extends string> = Rinha<Source> extends infer Result ? Result extends { value: infer Value } ? Value : Result : never;
-type RinhaStdOut<Source extends string> = Rinha<Source> extends infer Result ? Result extends { stdout: infer StdOut } ? StdOut : Result : never;
+type RinhaPrint<Source extends string> = Rinha<Source> extends infer Result ? Result extends { stdout: infer StdOut } ? StdOut : Result : never;
 
 // Numbers:
 
@@ -27,9 +27,9 @@ assert(_ as RinhaValue<"1024 * 1024 * 1024 * 1024 * 1024">, _ as 112589990684262
 assert(_ as RinhaValue<"1024 * 1024 * 1024 * 1024 * 1024 * 1024">, _ as 1152921504606846976n);
 assert(_ as RinhaValue<"1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024">, _ as 1180591620717411303424n);
 assert(_ as RinhaValue<"1180591620717411303424">, _ as 1180591620717411303424n);
-assert(_ as RinhaStdOut<"print(10)">, _ as "10\n");
-assert(_ as RinhaStdOut<"print(-14)">, _ as "-14\n");
-assert(_ as RinhaStdOut<"print(1180591620717411303424)">, _ as "1180591620717411303424\n");
+assert(_ as RinhaPrint<"print(10)">, _ as "10\n");
+assert(_ as RinhaPrint<"print(-14)">, _ as "-14\n");
+assert(_ as RinhaPrint<"print(1180591620717411303424)">, _ as "1180591620717411303424\n");
 
 assert(_ as RinhaValue<"1+">, _ as { $error: "Expected end of string, but found '+'" });
 
@@ -61,9 +61,18 @@ assert(_ as RinhaValue<`"hello"`>, _ as "hello");
 assert(_ as RinhaValue<`"a = " + 2`>, _ as "a = 2");
 assert(_ as RinhaValue<`10 + "a"`>, _ as "10a");
 
+// Print:
+
+assert(_ as RinhaValue<`print(1+1)`>, _ as 2);
+assert(_ as RinhaPrint<`let _ = print(1); print(2)`>, _ as "1\n2\n");
+assert(_ as RinhaPrint<"print(fn () => { 1 })">, _ as "<#closure>\n");
+assert(_ as RinhaPrint<"print(true)">, _ as "true\n");
+assert(_ as RinhaPrint<"print(false)">, _ as "false\n");
+assert(_ as RinhaPrint<`print("hello")`>, _ as "hello\n");
+assert(_ as RinhaPrint<`print("a = " + 2)`>, _ as "a = 2\n");
+
 // Functions:
 
-assert(_ as RinhaStdOut<"print(fn () => { 1 })">, _ as "<#closure>\n");
 assert(_ as RinhaValue<"let f = fn () => { 1 }; f()">, _ as 1);
 assert(_ as RinhaValue<"let a = 1; let f = fn () => { a }; let a = 2; f()">, _ as 1);
 assert(_ as RinhaValue<"let a = 1; let f = fn (a) => { a }; let a = 2; f(3)">, _ as 3);
@@ -71,6 +80,8 @@ assert(_ as RinhaValue<"let f = fn (n) => { if (n > 0) { f(n-1) } else { n } }; 
 assert(_ as RinhaValue<"(fn (sum) => { print(sum(10, 12)) })(fn (a, b) => { a + b })">, _ as 22);
 assert(_ as RinhaValue<"let execute = fn (func, n) => { func(n) }; let square = fn (n) => { n * n }; execute(square, 4)">, _ as 16);
 assert(_ as RinhaValue<"let fib = fn (n, a, b) => { if (n == 0) { a } else { fib(n - 1, b, a + b) } }; fib(100, 0, 1)">, _ as 354224848179261915075n);
+assert(_ as RinhaPrint<"let fib = fn (n, a, b) => { let _ = print(a); if (n == 0) { a } else { fib(n - 1, b, a + b) } }; fib(10, 0, 1)">, _ as "0\n1\n1\n2\n3\n5\n8\n13\n21\n34\n55\n");
+assert(_ as RinhaValue<"let f = fn () => { let x = 1; fn () => { x } }; let g = f(); g()">, _ as 1);
 
 // Semicolon:
 
@@ -78,3 +89,11 @@ assert(_ as RinhaValue<"1;">, _ as 1);
 assert(_ as RinhaValue<"1;;;;">, _ as 1);
 assert(_ as RinhaValue<"let f = fn () => { 1; }; f();">, _ as 1);
 assert(_ as RinhaValue<"let f = fn (x) => { if (x) { 1; } else { 2; } }; f(false);">, _ as 2);
+
+// Tuples:
+
+assert(_ as RinhaValue<"(1, 2)">, _ as [1, 2]);
+assert(_ as RinhaPrint<"print((1,2))">, _ as "(1, 2)\n");
+assert(_ as RinhaValue<"first((1, 2))">, _ as 1);
+assert(_ as RinhaValue<"second((1, 2))">, _ as 2);
+assert(_ as RinhaValue<`let reduce = fn (t, f) => { if (second(t) == 0) { first(t) } else { f(first(t), reduce(second(t), f)) } }; reduce((1, (2, (3, (4, 0)))), fn (a, b) => { a + b})`>, _ as 10);
